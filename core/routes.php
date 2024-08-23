5e765828eef799e73f48dfdfee9a4015bcd49865
Keshav Kumar <keshav0kumar@hotmail.com>
2024-08-23 23:12:53 +0530

    Routes defined

---
+++ core/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+$router->get("/", "controllers/home.php");
+
+$router->get("/todos", "controllers/show-all.php");
+$router->get("/todos/create", "controllers/add-todo.php");
+$router->post("/todos/create", "controllers/create.php");
+
+$router->get("/todo", "controllers/show.php");
+$router->get("/todo/edit", "controllers/edit-todo.php");
+$router->get("/todo/delete", "controllers/remove-todo.php");
+$router->patch("/todo", "controllers/update.php");
+$router->delete("/todo", "controllers/delete.php");
+
+$router->get("/register", "controllers/signup.php");
+$router->get("/login", "controllers/signin.php");
+
+$router->post("/register", "controllers/register.php");
+$router->post("/login", "controllers/login.php");
